c88375bd20759731b0b269e77e1f21f28583f579
Ivan Gechev <ivan_banov@abv.bg>
2023-03-22 18:26:27 +0200

    CompletionRequestBuilderTests is fully testd now

--- OpenAI.SDK.Tests/Completions/Models/Builders/CompletionRequestBuilderTests.cs
+++ OpenAI.SDK.Tests/Completions/Models/Builders/CompletionRequestBuilderTests.cs
@@ -6,15 +6,29 @@ namespace OpenAI.SDK.Tests.Completions.Models.Builders
 {
     public class CompletionRequestBuilderTests
     {
+        private readonly string _model = "some model";
+        private readonly string _prompt = "some prompt";
+        private readonly int _maxTokens = 1;
+        private readonly double _temperature = 1.0;
+        private readonly double _topP = 1.0;
+        private readonly int _n = 1;
+        private readonly bool _stream = true;
+        private readonly int _logProbs = 1;
+        private readonly string _stop = "some stop";
+
+        private readonly CompletionRequestBuilder _builder;
+
+        public CompletionRequestBuilderTests()
+        {
+            _builder = new CompletionRequestBuilder();
+        }
+
         [Fact]
         public void GivenModelAndPrompt_WhenBuildIsInvoked_ThenCompletionRequestIsReturned()
         {
-            var model = "some model";
-            var prompt = "some prompt";
-
-            var request = new CompletionRequestBuilder()
-                .WithModel(model)
-                .WithPrompt(prompt)
+            var request = _builder
+                .WithModel(_model)
+                .WithPrompt(_prompt)
                 .Build();
 
             request.Should().NotBeNull();
@@ -22,12 +36,37 @@ namespace OpenAI.SDK.Tests.Completions.Models.Builders
         }
 
         [Fact]
+        public void GivenValidParameters__WhenBuildIsInvoked_ThenCompletionRequestIsReturned()
+        {
+            var request = _builder
+                .WithModel(_model)
+                .WithPrompt(_prompt)
+                .WithMaxTokens(_maxTokens)
+                .WithTemperature(_temperature)
+                .WithTopP(_topP)
+                .WithN(_n)
+                .WithStream(_stream)
+                .WithLogProbs(_logProbs)
+                .WithStop(_stop)
+                .Build();
+
+            request.Should().NotBeNull();
+            request.Model.Should().Be(_model);
+            request.Prompt.Should().Be(_prompt);
+            request.MaxTokens.Should().Be(_maxTokens);
+            request.Temperature.Should().Be(_temperature);
+            request.TopP.Should().Be(_topP);
+            request.N.Should().Be(_n);
+            request.Stream.Should().Be(_stream);
+            request.LogProbs.Should().Be(_logProbs);
+            request.Stop.Should().Be(_stop);
+        }
+
+        [Fact]
         public void GivenNoModel_WhenBuildIsInvoked_ThenArgumentNullExceptionIsThrown()
         {
-            var prompt = "some prompt";
-
-            Action action = () => new CompletionRequestBuilder()
-                .WithPrompt(prompt)
+            Action action = () => _builder
+                .WithPrompt(_prompt)
                 .Build();
 
             action.Should().Throw<ArgumentNullException>();
@@ -36,10 +75,8 @@ namespace OpenAI.SDK.Tests.Completions.Models.Builders
         [Fact]
         public void GivenNoPrompt_WhenBuildIsInvoked_ThenArgumentNullExceptionIsThrown()
         {
-            var model = "some model";
-
-            Action action = () => new CompletionRequestBuilder()
-                .WithModel(model)
+            Action action = () => _builder
+                .WithModel(_model)
                 .Build();
 
             action.Should().Throw<ArgumentNullException>();
