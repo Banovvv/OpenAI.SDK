cb7cf8a6eff000d14ca123ca3389fae8c03e4ec8
Ivan Gechev <ivan_banov@abv.bg>
2023-03-23 17:40:45 +0200

    ImageResponse

---
+++ OpenAI.SDK/Common/Models/Data.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OpenAI.SDK.Common.Models
+{
+    /// <summary>
+    /// Data returned from the API.
+    /// </summary>
+    public class Data
+    {
+        /// <summary>
+        /// The url of the image returned by the API.
+        /// </summary>
+        [JsonPropertyName("url")]
+        public string? Url { get; set; }
+
+        /// <summary>
+        /// The base64-encoded image returned by the API.
+        /// </summary>
+        [JsonPropertyName("b64_json")]
+        public string? Base64Data { get; set; }
+    }
+}
--- OpenAI.SDK/Images/Models/ImageResponse.cs
+++ OpenAI.SDK/Images/Models/ImageResponse.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenAI.SDK.Common.Models;
+using System.Text.Json.Serialization;
 
 namespace OpenAI.SDK.Images.Models
 {
+    /// <summary>
+	/// Represents an image response returned by the API.  
+	/// </summary>
     public class ImageResponse
     {
+        /// <summary>
+        /// List of results of the embedding
+        /// </summary>
+        [JsonPropertyName("data")]
+        public List<Data>? Data { get; set; }
     }
 }
