26d85f63c76cbb69f9828e18701294d06aba5918
Ivan Gechev <ivan_banov@abv.bg>
2023-03-23 17:25:39 +0200

    ImageRequestBuilder

---
+++ OpenAI.SDK/Images/Builders/ImageRequestBuilder.cs
@@ -0,0 +1,68 @@
+﻿using OpenAI.SDK.Images.Models;
+
+namespace OpenAI.SDK.Images.Builders
+{
+    public class ImageRequestBuilder
+    {
+        private string? Prompt { get; set; }
+        private int? N { get; set; }
+        private string? Size { get; set; }
+        private string? ResponseFormat { get; set; }
+
+        /// <summary>
+        /// A text description of the desired image(s).
+        /// The maximum length is 1000 characters.
+        /// </summary>
+        public ImageRequestBuilder WithPrompt(string prompt)
+        {
+            Prompt = prompt;
+            return this;
+        }
+
+        /// <summary>
+        /// How many completions to generate for each prompt.
+        /// Must be between 1 and 10.
+        /// Defaults to 1.
+        /// </summary>
+        public ImageRequestBuilder WithN(int n)
+        {
+            N = n;
+            return this;
+        }
+
+        /// <summary>
+        /// The size of the generated images.
+        /// Must be one of 256x256, 512x512, or 1024x1024.
+        /// Defaults to 1024x1024.
+        /// </summary>
+        public ImageRequestBuilder WithSize(string size)
+        {
+            Size = size;
+            return this;
+        }
+
+        /// <summary>
+        /// The format in which the generated images are returned.
+        /// Must be one of <paramref name="url"/> or <paramref name="b64_json"/>.
+        /// </summary>
+        public ImageRequestBuilder WithResponseFormat(string responseFormat)
+        {
+            ResponseFormat = responseFormat;
+            return this;
+        }
+
+        /// <summary>
+		/// Creates a new <see cref="ImageRequest"/> with the specified parameters
+		/// </summary>
+        public ImageRequest Build()
+        {
+            ArgumentNullException.ThrowIfNull(Prompt, nameof(Prompt));
+
+            return new ImageRequest(
+                Prompt,
+                N,
+                Size,
+                ResponseFormat);
+        }
+    }
+}
--- OpenAI.SDK/Images/Models/ImageRequest.cs
+++ OpenAI.SDK/Images/Models/ImageRequest.cs
@@ -22,7 +22,8 @@ namespace OpenAI.SDK.Images.Models
         }
 
         /// <summary>
-        /// A text description of the desired image(s). The maximum length is 1000 characters.
+        /// A text description of the desired image(s).
+        /// The maximum length is 1000 characters.
         /// </summary>
         [JsonPropertyName("prompt")]
         public string Prompt { get; set; }
@@ -37,7 +38,8 @@ namespace OpenAI.SDK.Images.Models
         public int? N { get; set; }
 
         /// <summary>
-        /// The size of the generated images. Must be one of 256x256, 512x512, or 1024x1024.
+        /// The size of the generated images.
+        /// Must be one of 256x256, 512x512, or 1024x1024.
         /// Defaults to 1024x1024.
         /// </summary>
         [JsonPropertyName("size")]
@@ -45,7 +47,8 @@ namespace OpenAI.SDK.Images.Models
         public string? Size { get; set; }
 
         /// <summary>
-        /// The format in which the generated images are returned. Must be one of <paramref name="url"/> or <paramref name="b64_json"/>.
+        /// The format in which the generated images are returned.
+        /// Must be one of <paramref name="url"/> or <paramref name="b64_json"/>.
         /// </summary>
         [JsonPropertyName("response_format")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
